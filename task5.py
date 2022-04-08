fdb627fac46c2f7655de731bf506775d6929f3f2
Alexandr Mednikov <7910213419@mail.ru>
2022-04-08 20:25:19 +0300

    changehomework

--- task5.py
+++ task5.py
@@ -1,8 +1,10 @@
 rating = [7, 5, 3, 2, 2, 1]
 x = int(input("Введите число:"))
+
 for i in rating:
-    if x>i:
+    if x > i:
         rating.insert(rating.index(i), x)
         break
-
+if x <= rating[-1]:
+    rating.append(x)
 print(rating)
